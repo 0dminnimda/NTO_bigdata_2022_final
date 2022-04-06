e3fbb1c684e9be4baf362ae8bf0c73652e8b7b21
0dminnimda <0dminnimda@gmail.com>
2022-04-06 07:58:23 +0300

    functions.py: add plot_features_vs_label()

--- functions.py
+++ functions.py
@@ -12,5 +12,19 @@ def plot_the_loss_curve(epochs, error):
 
     plt.plot(epochs, error, label="Loss")
     plt.legend()
-    plt.ylim([error.min()*0.94, error.max()* 1.05])
+    plt.ylim([error.min()*0.94, error.max()*1.05])
     plt.show()
+
+
+def plot_features_vs_label(features, label):
+    for feature in features:
+        plt.figure()
+        # plt.xlabel("Epoch")
+        # plt.ylabel("Root Mean Squared Error")
+
+        plt.plot(feature, label)
+        plt.legend()
+        plt.ylim([label.min()*0.94, label.max()*1.05])
+        plt.xlim([label.min()*0.94, label.max()*1.05])
+        plt.show()
+
