16cedce9a060b8b1d7b4cce745f37b83cb8ec539
0dminnimda <0dminnimda@gmail.com>
2022-04-06 07:54:35 +0300

    functions.py: add plot_the_loss_curve()

--- functions.py
+++ functions.py
@@ -1,2 +1,16 @@
+from matplotlib import pyplot as plt
+
+
 def test():
     print(69)
+
+
+def plot_the_loss_curve(epochs, error):
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Root Mean Squared Error")
+
+    plt.plot(epochs, error, label="Loss")
+    plt.legend()
+    plt.ylim([error.min()*0.94, error.max()* 1.05])
+    plt.show()
